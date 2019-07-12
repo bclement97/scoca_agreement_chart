a485dcc8cf958d435960004d2fe47cef2f829946
Brandon Clement <17510591+bclement97@users.noreply.github.com>
2019-07-12 14:53:36 -0700

    Refactor

--- constants/constants.py
+++ constants/constants.py
@@ -1,10 +1,10 @@
-FIRST_OPINION_DATE = '2019-01-03'  # This is the start of the newest Assoc. Justice's tenure (J. Groban).
+DEFAULT_START_DATE = '2019-01-03'  # This is the start of the newest Assoc. Justice's tenure (J. Groban).
 DATE_FORMAT = '%Y-%m-%d'
 
 DOCKET_LIST_ENDPOINT = 'https://www.courtlistener.com/api/rest/v3/dockets/'
 DOCKET_LIST_FILTERS = {
     'court': 'cal',
-    'clusters__date_filed__gte': FIRST_OPINION_DATE,
+    'clusters__date_filed__gte': DEFAULT_START_DATE,
     'order_by': ['-date_modified', '-date_created'],
 }
 OPINION_CLUSTER_ENDPOINT = 'https://www.courtlistener.com/api/rest/v3/clusters/{}/'  # {} is ID
