38fb64ce45141930e0fc2b9a1f478e91f37c70b0
Brandon Clement <17510591+bclement97@users.noreply.github.com>
2019-07-30 12:21:38 -0700

    Creates utility function to return next opinion posting date

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import re
 import urllib
@@ -64,3 +65,41 @@ def get_response_json(response):
         raise NotImplementedError  # TODO
     except ValueError:
         raise NotImplementedError  # TODO
+
+
+def next_posting_date(ref_date=datetime.datetime.now()):
+    """Returns the next posting date for published SCOCA opinions (Monday/Thursday, 10am) after REF_DATE.
+    """
+    MON, TUE, WED, THU, FRI, SAT, SUN = range(7)
+
+    def next_day(day):
+        """Returns the date of the next occurrence of DAY (monday=0 ... sunday=6) or the current date if DAY is today.
+        """
+        days_delta = (day - ref_date.weekday()) % 7
+        dt_next_day = ref_date + datetime.timedelta(days=days_delta)
+        return dt_next_day.date()
+
+    def date_at_10am(date):
+        """Returns DATE at 10am.
+        """
+        time_10am = datetime.time(10)
+        return datetime.datetime.combine(date, time_10am)
+
+    next_monday_10am = date_at_10am(next_day(MON))
+    next_thursday_10am = date_at_10am(next_day(THU))
+
+    timedelta_monday_10am = next_monday_10am - ref_date
+    timedelta_thursday_10am = next_thursday_10am - ref_date
+
+    no_timedelta = datetime.timedelta()
+
+    # If one datetime is at or before now, select the other datetime
+    if timedelta_monday_10am <= no_timedelta:
+        return next_thursday_10am
+    elif timedelta_thursday_10am <= no_timedelta:
+        return next_monday_10am
+    # Otherwise, choose the closest one
+    elif timedelta_monday_10am < timedelta_thursday_10am:
+        return next_monday_10am
+    else:
+        return next_thursday_10am
