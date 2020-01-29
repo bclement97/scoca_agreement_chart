12b1ec18ec23d016ced7de752bc501630e6c7b3c
Brandon Clement <17510591+bclement97@users.noreply.github.com>
2020-01-29 12:02:42 -0800

    Allow OpinionType to be compared (equality) to ints

--- models.py
+++ models.py
@@ -160,6 +160,13 @@ class OpinionType(Enum):
     DISSENTING = 3
     CONCURRING_AND_DISSENTING = 4
 
+    def __eq__(self, other):
+        if type(other) is int:
+            return self.value == other
+        elif self.__class__ is other.__class__:
+            return self.value == other.value
+        return NotImplemented
+
     def __str__(self):
         return self.name.lower().replace('_', ' ')
 
